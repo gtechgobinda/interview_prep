9ddba73ac054232f36ef617bf619ab9ee51cf783
gobindatechwens <gobinda.saha@techwens.in>
2026-06-23 11:28:10 +0530

    done

--- 17_array_method.js
+++ 17_array_method.js
@@ -45,7 +45,8 @@ const numbers=[1,2,3,4,5]
 const forEachNumbers=numbers.forEach((item)=>{
   return item*2
 })
-console.log(forEachNumbers,"forEachNumbers") //undefined forEachNumbers
+console.log(forEachNumbers,"forEachNumbers") //undefined forEachNumbers 
+
 
 //*------find()
 // array.find((element,index,array)=>{
